5f44107a9974aef20e4f9167ccae85025dd66d65
devrunata <atanur-oeztuerk@gmx.de>
2012-08-19 18:09:29 +0200

    Fix microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,5 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   
   belongs_to :user
-  
-  validates :content,:length=>{maximum=>140}
+  validates :content,:length=>{:maximum=>140}
 end
